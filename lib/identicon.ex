2f9ede06523cbe568a61fb43e16ad57fa14feea1
Zafar <mdabuzafar78@gmail.com>
2018-05-16 14:17:15 +0530

    Save generated Image to Disk

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -7,17 +7,23 @@ defmodule Identicon do
     |> filter_odd_squares
     |> build_pixed_map
     |> draw_image
+    |> save_image(input)
+  end
+
+  def save_image(image, input) do
+    File.write("#{input}.png", input)
   end
 
   def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
     image = :egd.create(250, 250)
-    fill = :end.color(color)
+    fill = :egd.color(color)
 
     Enum.each pixel_map, fn({start, stop}) ->
       :egd.filledRectangle(image, start, stop, fill)
     end
 
     :egd.render(image)
+   end
 
   def build_pixed_map(%Identicon.Image{ grid: grid } = image) do
     pixel_map = Enum.map grid, fn ({ _code, index }) ->
