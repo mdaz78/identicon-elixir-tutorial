3a1d518c53ee9f63436d6a9c4097d81ffb8d44b2
Zafar <mdabuzafar78@gmail.com>
2018-05-16 11:48:02 +0530

    Add method to find the color

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -2,6 +2,13 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
+    |> pick_color
+  end
+
+  def pick_color(%Identicon.Image{ hex: [ r, g, b | _tail ] } = image) do
+    # %Identicon.Image{ hex: [ r, g, b | _tail ] } = image
+    # [ r, g, b | _tail ] = hex_list
+    %Identicon.Image{ image | color: { r, g, b } }
   end
 
   def hash_input(input) do
